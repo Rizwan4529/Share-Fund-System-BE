ea4d0483899adcddf06faf6f01763669b078bbd0
rizwan4529 <rizwikhattak4384@gmail.com>
2026-08-27 18:13:59 +0500

    [FEAT] Users CRUD

---
+++ src/features/users/user.controller.js
@@ -0,0 +1,27 @@
+import { HTTP_STATUS } from "#/utils/constants.js";
+import {
+  createUser,
+  resendLink,
+  loginUser,
+  validateUserEmail,
+} from "./user.service.js";
+
+export const loginUser = async (req, res) => {
+  const response = await userService.loginUser(req.body);
+  return res.status(HTTP_STATUS.OK).json(response);
+};
+
+export const registerUser = async (req, res) => {
+  const response = await createUser(req.body);
+  return res.status(HTTP_STATUS.CREATED).json(response);
+};
+
+export const verifyEmail = async (req, res) => {
+  const response = await validateUserEmail(req.body);
+  res.status(HTTP_STATUS.OK).json(response);
+};
+
+export const resendVerificationLink = async (req, res) => {
+  //TODO impliment the check where the user cant send more than 5 emails within 5 mins or So
+  const response = await resendLink(req.data);
+};
